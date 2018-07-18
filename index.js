31ef8e2432c493a2ff5718d0571b043eab485965
Haley Someone <hbarlar1@gmail.com>
2018-07-18 19:18:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   var newArray = [];
   do {
-    newArray.push()
-  }
-  
+    newArray.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  return newArray;
 }
